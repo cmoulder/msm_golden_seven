fae7123696ae43c8dd9056fad26246ac6457b153
Chris Moulder <cmoulder@chicagobooth.edu>
2017-04-30 18:02:21 -0500

    final

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,51 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+    render("/movies/show.html.erb")
+  end
+
+  def new_form
+    render("/movies/new_form.html.erb")
+  end
+
+  def create_row
+    @movie=Movie.new
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+
+    @movie.save
+
+    redirect_to("/movies/#{@movie.id}")
+  end
+
+  def edit_form
+    @movie = Movie.find(params[:id])
+    render("/movies/edit_form.html.erb")
+  end
+
+  def update_row
+    @movie = Movie.find(params[:id])
+    @movie.title = params[:title]
+    @movie.year = params[:year]
+    @movie.duration = params[:duration]
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+    @movie.save
+
+    redirect_to("/movies/#{@movie.id}")
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to("/movies")
+
+  end
+end
